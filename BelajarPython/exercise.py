183de0da8bd3da8c8b694b3609ea7cc94477faf0
Juang999 <juggernaut.9909@gmail.com>
2021-04-15 09:11:25 +0700

    update pembelajaran

---
+++ BelajarPython/exercise.py
@@ -0,0 +1,4 @@
+document = [{"Nama":"Juang", "Umur":"21", "Profesi":"Programmer"}, {"Nama":"Genta", "Umur":"20", "Profesi":"Programmer"}, {"Nama":"Fadil", "Umur":"20", "Profesi":"Programmer"}]
+
+for doc in document[1]:
+    print()
