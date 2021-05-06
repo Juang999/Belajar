9f36b0c940930f5195d0c296633e18cba77dcd79
Juang666 <juggernaut.9909@gmail.com>
2021-05-06 14:15:42 +0700

    update pembelajaran

---
+++ BelajarPython/CalMath/math.py
@@ -0,0 +1,27 @@
+def titikPuncak():
+    a = int(input("masukkan nilai a : "))
+    b = int(input("masukkan nilai b : "))
+    c = int(input("masukkan nilai c : "))
+
+    print("\n")
+    print("soal : ")
+    print(f"y = {a}x** + {b}x + {c}")
+
+    print("\n")
+    print("mencari nilai x")
+    x = -b/(2*a)
+    print(f"nilai x adalah = {x}")
+
+    print("\n")
+    print("penyelesaian")
+    print(f"y = ({a}.{x}**) + ({b}.{x}) + {c}")
+
+    a1 = a * (x**2)
+    b1 = b * x
+
+    hasil = a1 + b1 + c
+
+    print(f"y = {hasil}")
+
+def linear():
+
